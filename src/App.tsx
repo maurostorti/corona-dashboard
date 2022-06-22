ed5da3df91fe9e838165c5be1510a711e14af830
Mauro Storti <m.storti@student.tudelft.nl>
2022-06-22 18:21:27 +0200

    Setup Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { api } from './utils/api'
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
+  const [confirmed, setConfirmed] = useState<string>('0')
+  const [recovered, setRecovered] = useState<string>('10')
+  const [deaths, setDeaths] = useState<string>('0')
+
+  const covidData = () => {
+    api
+      .get('api')
+      .then((response) => {
+        const {
+          confirmed: { value: confirmedValue },
+          recovered: { value: recoveredValue },
+          deaths: { value: deathsValue },
+        } = response.data
+        setConfirmed(confirmedValue)
+        setRecovered(recoveredValue)
+        setDeaths(deathsValue)
+      })
+      .catch((error) => console.error(`Error: ${error}`))
+  }
+
+  useEffect(() => {
+    // setTimeout(() => {
+    covidData()
+  })
+
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+        <h1> Corona Dashboard </h1>
       </header>
+      <body className='App-body'>
+        <div className='Dashboard'>
+          <div className='Data-cell'>
+            <h2 className='Data-title'>Confimed Cases</h2>
+            <p>{confirmed}</p>
+          </div>
+          <div className='Data-cell'>
+            <h2 className='Data-title'>Recovered Cases</h2>
+            <p>{recovered}</p>
+          </div>
+          <div className='Data-cell'>
+            <h2 className='Data-title'>Deaths</h2>
+            <p>{deaths}</p>
+          </div>
+        </div>
+      </body>
     </div>
   )
 }
